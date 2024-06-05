dd1515ae7ba5e245dcaf4db79e21390df573593a
VinhmqDev <vinhmq.work@gmail.com>
2024-06-05 07:55:57 +0700

    update slipt function uploading file

--- src/services/quizzzServices.ts
+++ src/services/quizzzServices.ts
@@ -5,51 +5,6 @@ import path from "path";
 import csv from "csv-parser";
 import fs from "fs-extra";
 import convertExcelToJson from "convert-excel-to-json";
-
-const handleError = (err: any) => {
-  let errors: { [key: string]: string } = {
-    text: "",
-    answer_fc: "",
-  };
-  if (err.message) {
-    Object.values(err.errors).forEach((error: any) => {
-      errors[error.properties.path] = error.properties.message;
-    });
-  }
-  return errors;
-};
-export const getAllQuizzz = async (req: Request, res: Response) => {
-  try {
-    const quizzzes = await Quizzz.find();
-    res.status(200).json(quizzzes);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
-export const getQuizzz = async (req: Request, res: Response) => {
-  const { quizzzId } = req.params;
-  try {
-    const quizzz = await Quizzz.findOne({ _id: quizzzId });
-    if (!quizzz) {
-      return res.status(404).json({ message: "Quizzz not found" });
-    }
-    res.status(200).json(quizzz);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
-
-export const createQuizzz = async (req: Request, res: Response, next: any) => {
-  const quizzz = req.body;
-  try {
-    const newQuizzz = await new Quizzz(quizzz).save();
-    res.status(201).json(newQuizzz);
-  } catch (err) {
-    const errors = handleError(err);
-    res.status(400).json(errors);
-  }
-};
-
 const insertQuizzzes = async (quizzzes: any, res: Response) => {
   try {
     const newQuizzzes = await Quizzz.insertMany(quizzzes);
@@ -101,18 +56,53 @@ const handleExcelFile = async (filePath: string, res: Response) => {
   await insertQuizzzes(transformedData, res);
 };
 
-// // Function to insert quizzzes into the database
-// const insertManyQuizzzes = async (quizzzes: any[], res: Response) => {
-//   try {
-//     const newQuizzzes = await Quizzz.insertMany(quizzzes);
-//     res.status(201).json(newQuizzzes);
-//   } catch (err) {
-//     const errors = handleError(err);
-//     res.status(400).json(errors);
-//   }
-// };
-
 const upload = multer({ dest: "uploads/" });
+
+const handleError = (err: any) => {
+  let errors: { [key: string]: string } = {
+    text: "",
+    answer_fc: "",
+  };
+  if (err.message) {
+    Object.values(err.errors).forEach((error: any) => {
+      errors[error.properties.path] = error.properties.message;
+    });
+  }
+  return errors;
+};
+
+export const getAllQuizzz = async (req: Request, res: Response) => {
+  try {
+    const quizzzes = await Quizzz.find();
+    res.status(200).json(quizzzes);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+export const getQuizzz = async (req: Request, res: Response) => {
+  const { quizzzId } = req.params;
+  try {
+    const quizzz = await Quizzz.findOne({ _id: quizzzId });
+    if (!quizzz) {
+      return res.status(404).json({ message: "Quizzz not found" });
+    }
+    res.status(200).json(quizzz);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+export const createQuizzz = async (req: Request, res: Response, next: any) => {
+  const quizzz = req.body;
+  try {
+    const newQuizzz = await new Quizzz(quizzz).save();
+    res.status(201).json(newQuizzz);
+  } catch (err) {
+    const errors = handleError(err);
+    res.status(400).json(errors);
+  }
+};
 
 // Main function to handle the createMultipleQuizzz request
 export const createMultipleQuizzz = async (req: Request, res: Response) => {
