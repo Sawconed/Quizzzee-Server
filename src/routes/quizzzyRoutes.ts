891b26b8c7058aeb338429d6c3a4d7e78dd4218f
VinhmqDev <vinhmq.work@gmail.com>
2024-06-05 08:58:06 +0700

    make feature block and unblock

--- src/routes/quizzzyRoutes.ts
+++ src/routes/quizzzyRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
-   getAllQuizzzy, getQuizzzy, createQuizzzy, updateQuizzzy,deleteQuizzzy
+   getAllQuizzzy, getQuizzzy, createQuizzzy, updateQuizzzy,deleteQuizzzy,
+   blockQuizzzy, unblockQuizzzy
 } from "../services/quizzzyServices"
 
 const quizzzyRoutes = Router();
@@ -10,5 +11,6 @@ quizzzyRoutes.get("/:quizzzyId", getQuizzzy)
 quizzzyRoutes.post("/", createQuizzzy)
 quizzzyRoutes.put("/:quizzzyId", updateQuizzzy)
 quizzzyRoutes.delete("/:quizzzyId", deleteQuizzzy)
-
+quizzzyRoutes.patch("/:quizzzyId/block", blockQuizzzy);
+quizzzyRoutes.patch("/:quizzzyId/unblock", unblockQuizzzy);
 export default quizzzyRoutes;
