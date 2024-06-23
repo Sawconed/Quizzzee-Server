579da1fad5e608332a0fc463f438ecf71dcfc519
GiaBruh <baohngce171023@fpt.edu.vn>
2024-06-23 16:43:33 +0700

    chore: Update Quizzz model

--- src/models/Quizzz.ts
+++ src/models/Quizzz.ts
@@ -34,13 +34,12 @@ const quizzzSchema = new mongoose.Schema(
     text: {
       type: String,
       required: [true, "Please provide a question"],
-      minLength: [6, "Question must be at least 6 characters"],
+      minLength: [4, "Question must be at least 4 characters"],
       maxLength: [255, "Question must be at most 255 characters"],
     },
     answer_fc: {
       type: String,
-      required: [true, "Please provide a correct answer"],
-      minLength: [2, "Answer must be at least 2 characters"],
+      required: [true, "Please provide a suitable answer"],
       maxLength: [255, "Answer must be at most 255 characters"],
     },
   },
