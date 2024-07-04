08817beef1f4f149ae6add195ca855a7b6f10404
Talter <ToryNothing@gmail.com>
2024-07-04 16:42:31 +0700

    Report Service - Report patch

--- src/routes/reportRoutes.ts
+++ src/routes/reportRoutes.ts
@@ -3,6 +3,7 @@ import {
   createReport,
   deleteReport,
   getReportById,
+  resolveReport,
   getReports,
 } from "../services/reportServices";
 import {
@@ -18,6 +19,8 @@ reportRoutes.get("/", verifyJWT, verifyAdmin, getReports);
 reportRoutes.get("/:reportId", verifyJWT, verifyAdmin, getReportById);
 
 reportRoutes.post("/", verifyJWT, verifyUser, createReport);
+
+reportRoutes.patch("/", verifyJWT, verifyAdmin, resolveReport);
 
 reportRoutes.delete("/:reportId", verifyJWT, verifyAdmin, deleteReport);
 
--- src/services/reportServices.ts
+++ src/services/reportServices.ts
@@ -1,68 +1,149 @@
 import { Request, Response } from "express-serve-static-core";
 import Report from "../models/Report";
+import User from "../models/User";
 
 const handleError = (err: any) => {
-    let errors: { [key: string]: string } = { quizzzyId: "", createdBy: "", message: "" };
+  let errors: { [key: string]: string } = {
+    quizzzyId: "",
+    createdBy: "",
+    message: "",
+  };
 
-    if (err.message.includes("validation failed")) {
-        Object.values(err.errors).forEach((error: any) => {
-            errors[error.properties.path] = error.properties.message;
-        })
-    }
+  if (err.message.includes("validation failed")) {
+    Object.values(err.errors).forEach((error: any) => {
+      errors[error.properties.path] = error.properties.message;
+    });
+  }
 
-    return errors;
-}
+  return errors;
+};
 
 export const getReports = async (req: Request, res: Response) => {
-    try {
-        const reports = await Report.find();
-        res.status(200).json(reports);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-}
+  try {
+    const reports = await Report.find();
+    res.status(200).json(reports);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
 export const getReportById = async (req: Request, res: Response) => {
-    const { reportId } = req.params;
+  const { reportId } = req.params;
 
-    try {
-        const report = await Report.findById(reportId);
+  try {
+    const report = await Report.findById(reportId);
 
-        if (!report) {
-            return res.status(404).json({ message: "Report not found" });
-        }
-
-        res.status(200).json(report);
-    } catch (error) {
-        res.status(400).json(error);
+    if (!report) {
+      return res.status(404).json({ message: "Report not found" });
     }
-}
+
+    res.status(200).json(report);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
 export const createReport = async (req: Request, res: Response) => {
-    const newReport = new Report(req.body);
+  const newReport = new Report(req.body);
 
-    try {
-        const report = await newReport.save();
+  try {
+    const report = await newReport.save();
 
-        res.status(201).json(report);
-    } catch (error) {
-        const errors = handleError(error);
-        res.status(400).json(errors);
-    }
-}
+    res.status(201).json(report);
+  } catch (error) {
+    const errors = handleError(error);
+    res.status(400).json(errors);
+  }
+};
 
 export const deleteReport = async (req: Request, res: Response) => {
-    const { reportId } = req.params;
+  const { reportId } = req.params;
 
-    try {
-        const deletedReport = await Report.findByIdAndDelete(reportId);
+  try {
+    const deletedReport = await Report.findByIdAndDelete(reportId);
 
-        if (!deletedReport) {
-            return res.status(404).json({ message: "Report not found" });
-        }
-
-        res.status(200).json({ message: "Report deleted successfully" });
-    } catch (error) {
-        res.status(400).json(error);
+    if (!deletedReport) {
+      return res.status(404).json({ message: "Report not found" });
     }
-}
+
+    res.status(200).json({ message: "Report deleted successfully" });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const resolveReport = async (req: Request, res: Response) => {
+  const resolvedReport = new Report(req.body);
+
+  if (!resolvedReport || resolvedReport.status === "Not Checked") {
+    res.status(403).json({ message: "Report has not been resolved yet" });
+    return;
+  }
+  switch (resolvedReport.status) {
+    case "Deny Report": {
+      await denyReport(resolvedReport, res);
+      break;
+    }
+    case "Block User": {
+      await resovledReportBlockUser(resolvedReport, res);
+      break;
+    }
+    default: {
+      res.status(400).json({ message: "Invalid report status" });
+    }
+  }
+  return;
+};
+
+const denyReport = async (resolvedReport: any, res: Response) => {
+  try {
+    const report = await Report.findByIdAndUpdate(
+      resolvedReport._id,
+      resolvedReport,
+      {
+        new: true,
+      }
+    );
+    if (!report) {
+      res.status(404).json({ message: "Report not found" });
+    }
+    res.status(200).json({ message: "Report resolved" });
+  } catch (err) {
+    res.status(404).json(err);
+  }
+};
+
+const resovledReportBlockUser = async (resolvedReport: any, res: Response) => {
+  if (!resolvedReport) {
+    res.status(404).json({ message: "Report not found" });
+    return;
+  }
+  try {
+    const blockedUser = await User.findByIdAndUpdate(resolvedReport.createdBy, {
+      isActive: false,
+    });
+    if (!blockedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    try {
+        const report = await Report.findByIdAndUpdate(
+          resolvedReport._id,
+          resolvedReport,
+          {
+            new: true,
+          }
+        );
+        if (!report) {
+          res.status(404).json({ message: "Report not found" });
+        }
+        res.status(200).json({ message: "Report resolved" });
+      } catch (err) {
+        await User.findByIdAndUpdate(resolvedReport.createdBy, {
+            isActive: true,
+          });
+        res.status(404).json(err);
+      }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
